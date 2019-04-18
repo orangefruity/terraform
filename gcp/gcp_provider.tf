0e6dceca26311da728d0458eba42765b578a9e3e
Your Name <you@example.com>
2019-04-18 16:41:42 +0530

    Adding the terrform file for compute instance

--- gcp/gcp_provider.tf
+++ gcp/gcp_provider.tf
@@ -1,6 +1,6 @@
 provider "google" { 
-credentials = "${file("/home/anand/terraform/key2.json")}"
-project = "sil-rnd-231113"
+credentials = "${file("/home/anand/terraform/key4.json")}"
+project = "sentrifugo"
 region = "us-east1-b"
 }
 
--- gcp/gcp_resouce.tf
+++ gcp/gcp_resouce.tf
@@ -1,30 +1,18 @@
 resource "google_compute_instance" "centos7" {
 name = "elasticsearch1"
-machine_type = "g1-small"	
+machine_type = "n1-standard-2"	
 zone = "us-east1-b"
+
 	boot_disk {
 	initialize_params {
 	image = "centos-7-v20190326"
    		 }
           }
        network_interface {
-       network = "default"
+       network = "sent-vpc1"
+       subnetwork  = "sent-vpc1"
        access_config {
                      }
                          }
-
-}
-resource "google_compute_firewall" "default" {
-  name = "test-firewall"
-  network = "default"
- 
-     allow {
-       protocol = "icmp"
-     }
-
-     allow {
-       protocol = "tcp"
-       ports    = ["80", "443", "22"]
-     }
 
 }
